6adb924351164ce8cf56dec50f17537a70a6253b
vishnu <vishnuvikas.ganjai@eagles.oc.edu>
2022-10-10 14:00:27 -0400

    modifications

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+import store from './components/Store';
+import React from 'react'
+import { Provider } from 'react-redux'
+
+
+
+store.dispatch({
+  type: 'add',
+  data: {name : 'satish'}
+})
+store.subscribe(() =>{
+  console.log('in subscribe', store.getState());
+})
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-   <h1>hello</h1>
+      <Header/>
+      <Contacts/>
     </div>
+    </Provider>
   );
 }
 
